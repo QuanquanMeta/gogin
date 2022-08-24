be866e7c1707b37c9fd0dac66097c9d79b95ce60
Xiang.Wang <mikexiangwang@gmail.com>
2022-08-24 22:31:44 +0100

    gogin

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "value:%v", "hello go gin")
+	})
+
+	r.POST("/add", func(c *gin.Context) {
+		c.String(http.StatusOK, "value:%v", "POST")
+	})
+
+	r.PUT("/add", func(c *gin.Context) {
+		c.String(http.StatusOK, "value:%v", "PUT Edit")
+	})
+
+	r.DELETE("/add", func(c *gin.Context) {
+		c.String(http.StatusOK, "value:%v", "DELETE")
+	})
+
+	r.Run(":8800") // start a web service
+}
