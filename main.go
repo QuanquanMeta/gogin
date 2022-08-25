c5075020eb99972a64286894c88d195ce97fe1ff
mikexiangwang <mikexiangwang@gmail.com>
2022-08-25 19:47:25 +0100

    add source code

--- main.go
+++ main.go
@@ -11,18 +11,33 @@ import (
 func main() {
 	r := gin.Default()
 
+	// get
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "value:%v", "hello go gin")
+		c.String(http.StatusOK, "value:%v", "Front page")
 	})
 
+	r.GET("/json1", func(c *gin.Context) {
+		c.JSON(http.StatusOK, map[string]interface{}{
+			"success": true,
+			"msg":     "hello gin",
+		})
+	})
+
+	r.GET("/json2", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"success": true,
+			"msg":     "hello gin",
+		})
+	})
+	// post
 	r.POST("/add", func(c *gin.Context) {
 		c.String(http.StatusOK, "value:%v", "POST")
 	})
-
+	// put
 	r.PUT("/add", func(c *gin.Context) {
 		c.String(http.StatusOK, "value:%v", "PUT Edit")
 	})
-
+	// delete
 	r.DELETE("/add", func(c *gin.Context) {
 		c.String(http.StatusOK, "value:%v", "DELETE")
 	})
